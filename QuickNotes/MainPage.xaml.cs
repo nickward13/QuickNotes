81589c2712435b6a61a789fbf5f3dccdec81c1ed
Nick Ward <nickward13@hotmail.com>
2019-06-01 10:41:04 +1000

    Add project files.

---
+++ QuickNotes/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace QuickNotes
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            SetAuthState(false);
+
+            RootFrame.Navigate(typeof(HomePage));
+        }
+
+        private void SetAuthState(bool isAuthenticated)
+        {
+            (App.Current as App).IsAuthenticated = isAuthenticated;
+
+            // Toggle controls that require auth
+            Calendar.IsEnabled = isAuthenticated;
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            var invokedItem = args.InvokedItem as string;
+
+            switch (invokedItem.ToLower())
+            {
+                case "calendar":
+                    throw new NotImplementedException();
+                    break;
+                case "home":
+                default:
+                    RootFrame.Navigate(typeof(HomePage));
+                    break;
+            }
+        }
+    }
+}
